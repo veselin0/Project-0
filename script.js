321027114c12659dc61eab9ad5dd9efc812f3723
Veselin <vesselin60@gmail.com>
2020-08-10 12:40:48 +0300

    Project 0 Part 2

--- script.js
+++ script.js
@@ -21,6 +21,24 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+
+// Код возьмите из предыдущего домашнего задания
+
 'use strict';
 
 const numberOfFilms = Number(prompt('Сколько фильмов вы уже посмотрели?', ''));
@@ -33,12 +51,34 @@ const personalMovieDB = {
     private: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    const b = prompt('На сколько оцените его?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count != null && personalMovieDB.count != '' && personalMovieDB.count < 10) {
+    document.getElementById('demo').innerText = "Просмотрено довольно мало фильмов";
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    document.getElementById('demo').innerText = "Вы классический зритель";
+} else if (personalMovieDB.count > 30) {
+    document.getElementById('demo').innerText = "Вы киноман";
+} else {
+    document.getElementById('demo').innerText = "Произошла ошибка";
+}
+
+/* const a = prompt('Один из последних просмотренных фильмов?', '');
 const b = prompt('На сколько оцените его?', '');
 const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
+const d = prompt('На сколько оцените его?', ''); */
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//personalMovieDB.movies[a] = b;
+//personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
